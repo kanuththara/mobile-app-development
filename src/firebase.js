7cccf4e3406c35478251b86c7923606dc220828b
Kalani Anuththara <info.kanuththara@gmail.com>
2025-08-25 13:58:50 +0530

    Day 6 - React setup + basic screens + Firebase init

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKqlSwiWRWr_W0Aw86KRlvk6gX7unIb0Y",
+  authDomain: "mobile-app-development-3e884.firebaseapp.com",
+  projectId: "mobile-app-development-3e884",
+  storageBucket: "mobile-app-development-3e884.firebasestorage.app",
+  messagingSenderId: "832813235278",
+  appId: "1:832813235278:web:1d1d9c997d670cb2c2571f",
+  measurementId: "G-ST4MFZYH3X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Log to verify
+console.log("Firebase initialized:", app);
+
+const analytics = getAnalytics(app);
+export default app;
